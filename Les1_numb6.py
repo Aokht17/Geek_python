233ea3d0b94efe0f3bed0704f05bffb9c34d177f
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2021-02-08 19:28:44 +0300

    Delete Les1_numb6.py

--- Les1_numb6.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Сколько километров пробежал спортсмен в 1 день?"))
-b = float(input("Сколько должен пробежать спортсмен?"))
-day = 1
-while True:
-    if a >= b:
-        break
-    a *= 1.1
-    day += 1
-print(day)
