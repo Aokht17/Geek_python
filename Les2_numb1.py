7edefcc7009666e0665637881abcc017b3bef2b8
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2021-02-08 19:28:54 +0300

    Delete Les2_numb1.py

--- Les2_numb1.py
+++
@@ -1,8 +0,0 @@
-my_list = []
-my_list.append([True, 'spring', 45.8])
-my_list.extend([True, 'spring', 45.8, 90])
-my_list.insert(3, (5, ['abs'], False))
-for i in my_list:
-    print(type(i))
-
-print(my_list)
